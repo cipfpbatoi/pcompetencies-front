e15e876d27e517153320c174acb911747b1c4522
Alexandre Coloma <alexandre.coloma+edu@gmail.com>
2024-07-13 00:22:51 +0200

    [ADD] reload data on auth login user intranet

--- src/views/LoginIntranet.vue
+++ src/views/LoginIntranet.vue
@@ -6,13 +6,15 @@ import { useDataStore } from '../stores/data'
 export default {
   name: 'LoginIntranet',
   async mounted() {
-    const token = this.$route.params.token 
-//      'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwOi8vaW50cmFuZXQuY2lwZnBiYXRvaS5lcyIsImF1ZCI6Imh0dHA6Ly9wcm9ncmFtYWNpb25zLmNpcGZwYmF0b2kuZXMiLCJqdGkiOiJhNGJkODY4ZDMwYTkyNDU5NDIwMmQ1NTJhZWExYjVhNyIsImlhdCI6MTcxOTg0NjUwMy44MDYyNiwibmJmIjoxNzE5ODQ2NTAzLjgwNjI2LCJleHAiOjE3MTk4NTAxMDMuODA2MjYsInJvbGUiOlsiUk9MRV9VU0VSIiwiUk9MRV9URUFDSEVSIl0sIm1vZHVsZSI6bnVsbCwiY3ljbGUiOiJDRk0gU01YICAoTE9FKSIsInR1cm4iOiJwcmVzZW50aWFsIiwibmFtZSI6IkZyYW5jaXNjbyBHYXJjaWEgTWFyY28iLCJlbWFpbCI6ImYuZ2FyY2lhbWFyY29AZWR1Lmd2YS5lcyIsImRlcGFydG1lbnQiOiJJbmYifQ.T8IKq397QpKaVbeloYY8OgPDrd3FjD74XbY_a8w3yqu2Lx1w1NHOMdbfO2xgxxc-jdMn6XpKLKzaySczBuP_5NPENun_-POVS3wj_DPf7hw_Q8K7_PU08dSPRpghT4Fxj4_PDb-Hl6i5ihX7YII3ittb_b_zUHCStaH7pqQlaihmd9OVRYkA_H_2qhxGnj2GVxhHl2qIipMlP__yShkyLUo2ueHKB5QePgbHB9UFiuHM9UP9qnvKCrPsjvxUKWiVbtOZfOXi_YOUIfMdxUtFWQ3FzmAv2RA4kRC-m-IWNhObZrTRCXIVPTXTznQpdat_6jXl18ASeyMTGx5pCnJxcg'
+    this.isLoading = true;
+    const token = this.$route.params.token
     localStorage.removeItem('token')
     try {
       const response = await api.loginIntranet(token)
       localStorage.token = response.data.jwt
       await this.loadCurrentUser()
+      await this.loadData();
+      this.isLoading = false;
       this.$router.push({
         name: 'selectedSyllabus',
         params: {
@@ -23,15 +25,17 @@ export default {
     } catch (error) {
       this.addMessage('error', error)
       this.msg = error.detail || error.message
+      this.isLoading = false;
     }
   },
   data() {
     return {
-      msg: 'Espere per favor'
+      isLoading: false,
+      msg: 'Espere per favor...'
     }
   },
   methods: {
-    ...mapActions(useDataStore, ['addMessage', 'loadCurrentUser']),
+    ...mapActions(useDataStore, ['addMessage', 'loadCurrentUser', 'loadData']),
     login() {
       this.error = ''
       if (this.email === 'admin' && this.password === 'admin') {
@@ -47,8 +51,11 @@ export default {
 <template>
   <main class="border shadow view-main">
     <h2 class="text-center fw-bold text-primary p-2">Logejant usuari</h2>
-    <div class="container-fluid px-lg-4">
-      <p>{{ msg }}</p>
+    <div class="container-fluid px-lg-4 mt-5 text-center">
+      <em class="h4 m-3 text-primary-emphasis text-info-emphasis">{{ msg }}</em>
+      <div :class="{ 'd-none' : !this.isLoading }">
+        <span class="spinner-border text-primary mt-4"></span>
+      </div>
     </div>
   </main>
 </template>

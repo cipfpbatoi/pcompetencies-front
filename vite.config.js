22f48f6fcbe10098c01a57ef6b1911d52a4a9d88
Arturo Candela <frisix@gmail.com>
2026-07-02 11:06:20 +0200

    fix: proxy local API requests in development

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
